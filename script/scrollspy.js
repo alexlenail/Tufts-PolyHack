be38c18228bed919fc0524ce0382b24508e79a19
Alexander Lenail <alexander.lenail@tufts.edu>
2015-08-22 00:27:15 -0700

    bottom bar configured

---
+++ script/scrollspy.js
@@ -0,0 +1,23 @@
+$(document).ready(function(e) {
+
+  $("#nav ul li a[href^='#']").on('click', function(e) {
+
+     // prevent default anchor click behavior
+     e.preventDefault();
+
+     // store hash
+     var hash = this.hash;
+
+     // animate
+     $('html, body').animate({
+         scrollTop: $(hash).offset().top
+       }, 300, function(){
+
+         // when done, add hash to url
+         // (default click behaviour)
+         window.location.hash = hash;
+       });
+
+  });
+});
+
